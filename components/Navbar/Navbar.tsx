86e0479dffb8eaf01a2e7ede1fa4f8447461e619
naMqe <namqe07@gmail.com>
2024-07-01 13:15:48 +0200

    initial commit

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export const Navbar = () => {
+
+    return (
+        <div className="flex justify-between items-center p-4">
+            <h1 className="text-2xl font-bold">Crypto Analyzer</h1>
+            <div className="flex items-center gap-6">
+                <Link href="/" className="text-lg tracking-wider hover:text-[#dca54c]">Home</Link>
+                <Link href="/trending" className="text-lg tracking-wider hover:text-[#dca54c]">Trending</Link>
+                <Link href="/top-gainers-losers" className="text-lg tracking-wider hover:text-[#dca54c]">Top Gainers/Losers</Link>
+                <Link href="/market-cap-dominance" className="text-lg tracking-wider hover:text-[#dca54c]">Market Cap Dominance</Link>
+            </div>
+        </div>
+    )
+}
